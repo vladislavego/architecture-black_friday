78ab473a5d8b90a1d879cae6d9c2f833effc2d52
vladislavego <ego.v.a@ya.ru>
2026-01-05 23:08:46 +0100

    task2

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -6,6 +6,7 @@
 
 docker compose exec -T mongodb1 mongosh <<EOF
 use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+for (var i = 0; i < 1000; i++) {
+  db.helloDoc.insertOne({ age: i, name: "ly" + i })
+}
 EOF
-
